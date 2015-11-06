3b203504fa23caa5a36e5f1b67c5e4b2e4b14c63
Pin <pin@Lu-MacPro.local>
2015-11-06 14:26:05 -0500

    adding routes for sign up and in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 8, allow_nil: true}
   has_many :presents
   has_many :judgments
   has_many :toys, through: :presents
+  
 end
 
 # Every Child has many presents & many judgments Child is a type of user.
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  root 'application#welcome'
+
   resources :users
   resources :judgments
   resources :toys
   resources :presents
-  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
