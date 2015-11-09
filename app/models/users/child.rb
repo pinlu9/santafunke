d8703d2fa55300ea56b57edb8af81c690e5d839f
Andy Oh <andyoh2@gmail.com>
2015-11-08 22:25:01 -0500

    updated models for child and elf

--- app/models/users/child.rb
+++ app/models/users/child.rb
@@ -1,3 +1,6 @@
 class Child < User
-
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 8, allow_nil: true}
+  has_many :toys, through: :presents
 end
