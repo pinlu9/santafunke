d1416718e845f2861c8e99a99f4dd40fc59c8c76
Kevin Walter <kevinwalter0731@gmail.com>
2015-11-10 12:16:59 -0500

    Updated judgments table via migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109143049) do
+ActiveRecord::Schema.define(version: 20151110171422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,11 @@ ActiveRecord::Schema.define(version: 20151109143049) do
   create_table "judgments", force: :cascade do |t|
     t.integer  "child_id"
     t.integer  "elf_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.string   "description"
+    t.boolean  "naughty",           default: true
+    t.string   "qualifying_adverb"
   end
 
   create_table "presents", force: :cascade do |t|
