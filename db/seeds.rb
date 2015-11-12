b8f086c0f2052738a06c6f5cb3645ecbb4fe9750
Pin <pin@Lu-MacPro.local>
2015-11-12 12:01:43 -0500

    in order to remove conflict

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+def people
+    User.create(name:"Bob Black",
+                  email: "bob.black@ga.com",
+                  type: "Child",
+                  toy: "Boat")
+
+
+    # 20.times do |n|
+    #   n = Child.new
+    #   n.name = Faker::Name.name
+    #   n.email = Faker::Internet.email
+    #   n.save
+      # city = Faker::AddressUS.city_suffix
+      # zipcode  = Faker::AddressUS.zip_code
+      # state = Faker::AddressUS.state
+
+      10.times do |n|
+          n = Child.create
+          n.name = Faker::Name.name
+          n.email = Faker::Internet.email
+          n.password_digest = "test"
+          n.save
+
+    end
+  end
